b2971d368d6f60fe09a52db7e31e275d470faac6
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-09-02 15:43:25 +0200

    Testing

--- test/tc_phone_field.php
+++ test/tc_phone_field.php
@@ -3,9 +3,7 @@ class TcPhoneField extends TcBase {
 
 	function test(){
 		$this->field = new PhoneField(array("default_country_code" => "+420"));
-
-		// Valid numbers
-		foreach([
+		$this->_testValidNumbers([
 			"+420.605111222" => "+420.605111222",
 			"+420605111223" => "+420.605111223",
 			"420605111224" => "+420.605111224",
@@ -17,7 +15,26 @@ class TcPhoneField extends TcBase {
 			"+421.605333444" => "+421.605333444",
 			"+421 605 333 445" => "+421.605333445",
 			"421605333446" => "+421.605333446",
-		] as $input => $phone){
+		]);
+
+		$this->field = new PhoneField(array("default_country_code" => "+421"));
+		$this->_testValidNumbers([
+			"+420.605111222" => "+420.605111222",
+			"+420605111223" => "+420.605111223",
+			"420605111224" => "+420.605111224",
+			"420 605 111 225" => "+420.605111225",
+
+			"605 333 444" => "+421.605333444",
+			"605333445" => "+421.605333445",
+
+			"+421.605333444" => "+421.605333444",
+			"+421 605 333 445" => "+421.605333445",
+			"421605333446" => "+421.605333446",
+		]);
+	}
+
+	function _testValidNumbers($ary){
+		foreach($ary as $input => $phone){
 			$cleaned_phone = $this->assertValid($input);
 			$this->assertEquals($phone,$cleaned_phone);
 		}
